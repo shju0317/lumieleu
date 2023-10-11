6f40b97c7d7149bcac8ecd8f9fb68e29af6046f1
seobinbang7 <newmso5382@gmail.com>
2023-10-11 12:36:24 +0900

    markup: Cart.jsx 마크업 작업중

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,30 @@
+import S from './Cart.module.css';
+
 function Cart() {
   return (
-    <div>
-      <span>Cart</span>
+    <div className={S.cartWrapper}>
+      <h1 className="sr-only">CHECKOUT</h1>
+      <span
+        className="text-2xl font-bold mb-[4.5rem]"
+        aria-label="장바구니"
+        aria-hidden
+      >
+        C H E C K O U T
+      </span>
+      <ul className="flex">
+        <li className="mr-[20.625rem]">
+          <span className="font-semibold">Product</span>
+        </li>
+        <li className="mr-7">
+          <span className="font-semibold">Price</span>
+        </li>
+        <li className="mr-7">
+          <span className="font-semibold">Quantity</span>
+        </li>
+        <li>
+          <span className="font-semibold">Subotal</span>
+        </li>
+      </ul>
     </div>
   );
 }
