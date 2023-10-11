c1c1c39b5b16e5f3ed12b725bd64eb0b02768e8b
youngniiiiii <hanuje_@naver.com>
2023-10-11 12:15:01 +0900

    React : 라우트 signin 추가

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,9 +1,13 @@
 import S from './Heading.module.css';
+import { Link } from 'react-router-dom';
 
 function Heading() {
   return (
     <div className={S.headingWrapper}>
-      <h1 className='text-white'>헤더</h1>
+      <h1 className="text-white">헤더</h1>
+      <Link to="signin" className="text-white">
+        Sign In
+      </Link>
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,13 +5,15 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom';
 
-const RootLayout = lazy(() => import('./layout/RootLayout/RootLayout'));
-const Home = lazy(() => import('./pages/Home/Home'));
+const RootLayout = lazy(() => import('@/layout/RootLayout/RootLayout'));
+const Home = lazy(() => import('@/pages/Home/Home'));
+const SignIn = lazy(() => import('@/pages/SignIn/SignIn'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/lumieleu" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="signin" element={<SignIn />} />
     </Route>
   )
 );
