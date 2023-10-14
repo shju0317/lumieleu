0053d071c4909f8dabdffddaef9810802f487f7c
seobinbang7 <newmso5382@gmail.com>
2023-10-14 16:56:28 +0900

    react: sb-pages routes 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,13 +5,17 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom';
 
-const RootLayout = lazy(() => import('./layout/RootLayout/RootLayout'));
-const Home = lazy(() => import('./pages/Home/Home'));
+const RootLayout = lazy(() => import('@/layout/RootLayout/RootLayout'));
+const Home = lazy(() => import('@/pages/Home/Home'));
+const Cart = lazy(() => import('@/pages/Cart/Cart'));
+const Order = lazy(() => import('@/pages/Order/Order'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/lumieleu" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="order" element={<Order />} />
     </Route>
   )
 );
