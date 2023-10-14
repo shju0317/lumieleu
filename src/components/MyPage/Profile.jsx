831c2fbbfd6b0a20f52bc9d3ac82ffcb6e6a7df2
shju0317 <shju0317@naver.com>
2023-10-14 13:49:25 +0900

    markup: 프로필 컴포넌트 마크업

---
+++ src/components/MyPage/Profile.jsx
@@ -0,0 +1,26 @@
+function Profile() {
+  return (
+    <section className="flex justify-between border border-black rounded px-8 py-4 mx-auto max-w-3xl">
+      <h2 className="hidden">나의 정보</h2>
+      <div className="flex gap-4 items-center">
+        <img src="/public/profile.svg" alt="프로필" />
+        <div>
+          <p><strong>김회원</strong>님 안녕하세요!</p>
+          <button className="border border-black rounded px-7 py-1 mt-2">프로필 수정</button>
+        </div>
+      </div>
+      <div className="flex border-l border-black gap-6 pl-10">
+        <div className="flex flex-col gap-1 items-center">
+          <img src="/public/order.svg" alt="주문" />
+          <p>총 <strong>10</strong>건 주문</p>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <img src="/public/like.svg" alt="좋아요" />
+          <p><strong>5</strong>개 좋아요</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Profile
