b10a4a0b998f3c0a653e22c03e288fca1d066fbc
seobinbang7 <newmso5382@gmail.com>
2023-10-14 16:31:05 +0900

    react: routes 작업

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,12 +8,14 @@ import {
 const RootLayout = lazy(() => import('@/layout/RootLayout/RootLayout'));
 const Home = lazy(() => import('@/pages/Home/Home'));
 const Cart = lazy(() => import('@/pages/Cart/Cart'));
+const Order = lazy(() => import('@/pages/Order/Order'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/lumieleu" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="/lumieleu/:productId" element={<Cart />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="order" element={<Order />} />
     </Route>
   )
 );
