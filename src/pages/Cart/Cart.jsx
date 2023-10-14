18a864853588ac27ed26bacb1aa3329ed9c48738
seobinbang7 <newmso5382@gmail.com>
2023-10-14 16:29:57 +0900

    markup: orderList, selctedProduct, selectedProductItem, cart, order 파일 작업

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,45 +1,10 @@
-import pb from '@/api/pocketbase';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
 import S from './Cart.module.css';
+import SelectedProduct from '@/components/SelectedProduct/SelectedProduct';
 
-async function Cart() {
-  const { productId } = useParams();
-  console.log(productId);
-
-  const commentUser = await pb.collection('cart').getFullList();
-  console.log(commentUser);
-
+function Cart() {
   return (
     <div className={S.cartWrapper}>
-      <h1 className="sr-only">CHECKOUT</h1>
-      <span
-        className="text-2xl font-bold mb-[4.5rem]"
-        aria-label="장바구니"
-        aria-hidden
-      >
-        C H E C K O U T
-      </span>
-      <ul className="flex pb-2 mb-8 border-b-2 border-black">
-        <li className="mr-[56.26rem]">
-          <span className="font-semibold">Product</span>
-        </li>
-        <li className="mr-7">
-          <span className="font-semibold">Price</span>
-        </li>
-        <li className="mr-7">
-          <span className="font-semibold">Quantity</span>
-        </li>
-        <li>
-          <span className="font-semibold">Subotal</span>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <img alt="사진" src="" />
-          <span>Hello</span>
-        </li>
-      </ul>
+      <SelectedProduct />
     </div>
   );
 }
