ca794a8e3b843761cf53df45ff6be75faaaf3275
youngniiiiii <hanuje_@naver.com>
2023-10-19 23:18:05 +0900

    react : 비밀번호가 두개가 같지 않으면 오류처리 toast.error

--- src/pages/SignUs/SignUs.jsx
+++ src/pages/SignUs/SignUs.jsx
@@ -6,20 +6,60 @@ import I from '@/pages/SignUs/SignUs.module.css';
 // import debounce from "@/utils/debounce";
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 // import {useNavigate} from "react-router-dom";
 
 function SignUs() {
   const id = useId();
   /* Input 사용자 입력 값 감지 */
   const initalState = {
+    id: '',
     name: '',
     email: '',
     password: '',
     passwordConfirm: '',
+    phone: '',
+    address: '',
   };
   const [formState, setFormState] = useState(initalState);
+  const { name, email, password, passwordConfirm } = formState;
+
+  // input의 onChange
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  /* 회원가입 시 유효성 검사 */
+  const validateSignUp = () => {
+    // if (!pwReg(password)) {
+    //   toast.error(
+    //     "비밀번호는 10자리 이상, 14자리이하 하나의 알파벳 문자를 포함하는 특수문자를 입력해주세요!",
+    //     {icon: "😡"}
+    //   );
+    //   throw new Error(
+    //     "비밀번호는 10자리 이상, 14자리이하 하나의 알파벳 문자를 포함하는 특수문자를 입력해주세요!"
+    //   );
+    // }
+    // if (name === "name" && !engReg(name)) {
+    //   toast.error("닉네임은 영문으로만 입력해주세요!", {icon: "😡"});
+    //   throw new Error("닉네임은 영문으로만 입력해주세요!");
+    // }
+    if (password !== passwordConfirm) {
+      toast.error('비밀번호가 일치하지 않습니다!', { icon: '😡' });
+      throw new Error('비밀번호가 일치하지 않습니다!');
+    }
+  };
+
+  // 전송버튼 클릭시
   const handleRegister = (e) => {
     e.preventDefault();
+    // 비밀번호 일치하는지 확인
+    validateSignUp();
+
     // SDK인증요청
     console.log('가입');
   };
@@ -27,6 +67,7 @@ function SignUs() {
   return (
     <>
       <section className=" h-screen">
+        {/* 왼쪽영역 */}
         <div className=" p-24 bg-black text-white w-1/2 h-screen py-24 fixed top-0 left-0">
           <p className="absolute bottom-48 left-7 text-6xl font-light">
             lumière de l'aube
@@ -42,6 +83,7 @@ function SignUs() {
             <div className={S.solid}></div>
           </div>
         </div>
+        {/* 오른쪽영역 */}
         <div className=" w-1/2 float-right p-24 flex flex-col items-center justify-center bg-white z-10">
           <h2 className="text-2ㅍxl mb-8 text-[#454444]">회원가입</h2>
           {/* input */}
@@ -53,9 +95,13 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 아이디<span className="text-red-600"> *</span>
               </label>
+              {/* 아이디 */}
               <input
                 id={id}
                 type="text"
+                name="id"
+                onChange={handleInput}
+                value={formState.name}
                 placeholder="아이디를 입력해 주세요"
                 className={I.input}
               />
@@ -64,9 +110,13 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 비밀번호<span className="text-red-600"> *</span>
               </label>
+              {/* 패스워드 */}
               <input
                 id={id}
                 type="text"
+                name="password"
+                onChange={handleInput}
+                value={formState.password}
                 placeholder="비밀번호를 입력해 주세요"
                 className={I.input}
               />
@@ -75,10 +125,14 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 비밀번호 확인<span className="text-red-600"> *</span>
               </label>
+              {/* 패스워드 확인 */}
               <input
                 id={id}
                 type="text"
-                placeholder="비밀번호를 입력해 주세요"
+                name="passwordConfirm"
+                onChange={handleInput}
+                value={formState.passwordConfirm}
+                placeholder="비밀번호를 한번더 입력해 주세요"
                 className={I.input}
               />
             </div>
@@ -86,10 +140,14 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 이름<span className="text-red-600"> *</span>
               </label>
+              {/* 이름 */}
               <input
                 id={id}
                 type="text"
-                placeholder="비밀번호를 입력해 주세요"
+                name="name"
+                onChange={handleInput}
+                value={formState.name}
+                placeholder="이름을 입력해 주세요"
                 className={I.input}
               />
             </div>
@@ -97,10 +155,14 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 이메일<span className="text-red-600"> *</span>
               </label>
+              {/* e-mail */}
               <input
                 id={id}
                 type="text"
-                placeholder="비밀번호를 입력해 주세요"
+                name="email"
+                onChange={handleInput}
+                value={formState.email}
+                placeholder="이메일을 입력해 주세요"
                 className={I.input}
               />
             </div>
@@ -108,10 +170,13 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 휴대전화<span className="text-red-600"> *</span>
               </label>
+              {/* phone */}
               <input
                 id={id}
                 type="text"
-                placeholder="비밀번호를 입력해 주세요"
+                name="phone"
+                onChange={handleInput}
+                value={formState.phone}
                 className={I.input}
               />
             </div>
@@ -119,10 +184,13 @@ function SignUs() {
               <label htmlFor={id} className={I.label}>
                 주소<span className="text-red-600"> *</span>
               </label>
+              {/* 주소 */}
               <input
                 id={id}
                 type="text"
-                placeholder="비밀번호를 입력해 주세요"
+                name="address"
+                onChange={handleInput}
+                value={formState.address}
                 className={I.input}
               />
             </div>
