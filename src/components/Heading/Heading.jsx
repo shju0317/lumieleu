3eefc402bdfa7ad6ed7cd320fb4d8122d8ce328f
dongapple <hg971027@gmail.com>
2023-10-19 12:51:12 +0900

    markup : 해더 초기 생성

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,9 +1,22 @@
+import Nav from '@/layout/Nav/Nav';
 import S from './Heading.module.css';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Heading() {
+  const location = useLocation();
+  if (location.hash === '#home') {
+    localStorage.setItem('visitedPlaceOrMap', 'true');
+  }
+
   return (
-    <div className={S.headingWrapper}>
-      <h1 className='text-white'>헤더</h1>
+    <div
+      className={`${S.headingWrapper} ${
+        location.hash === '#home' ? S.textWhite : ''
+      }`}
+    >
+      <Link to="/lumieleu/">로고</Link>
+      <Nav className={location.hash === '#home' ? S.navTextWhite : ''} />
     </div>
   );
 }
--- src/layout/Nav/Nav.jsx
+++ src/layout/Nav/Nav.jsx
@@ -1,13 +1,43 @@
-import S from './Nav.module.css';
+// import S from './Nav.module.css';
+import { PropTypes } from 'prop-types';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-function Nav() {
+function Nav({ onCategorySelect }) {
+  const [selectedCategory, setSelectedCategory] = useState('전체');
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    onCategorySelect(category);
+  };
+
+  const categories = ['ABOUT', 'GALLERY', 'CART', 'MYPAGE', 'LOGOUT'];
+
   return (
-    <div>
-      <nav className={S.NavWrapper}>
-        <span className="text-white">FooterBar</span>
-      </nav>
-    </div>
+    <nav>
+      <ul className="flex flex-wrap gap-5 justify-evenly border-gray-1 top-13">
+        {categories.map((category) => (
+          <li key={category} className="">
+            <Link
+              to={`/lumieleu/${category.toLowerCase()}`}
+              onClick={() => handleCategoryClick(category)}
+              className={` py-2  ${
+                selectedCategory === category
+                  ? 'border-b-4 pb-1 box-border border-black '
+                  : ''
+              } `}
+            >
+              {category}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
 
 export default Nav;
+
+Nav.propTypes = {
+  onCategorySelect: PropTypes.func.isRequired,
+};
