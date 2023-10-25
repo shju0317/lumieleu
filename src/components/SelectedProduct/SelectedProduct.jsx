b11dd8d2ac0c218020036725e1e8816c69c3a943
seobinbang7 <newmso5382@gmail.com>
2023-10-25 16:46:49 +0900

    markup: orderList.jsx, selectedProduct.jsx, selctedProductitem.jsx 마크업

--- src/components/SelectedProduct/SelectedProduct.jsx
+++ src/components/SelectedProduct/SelectedProduct.jsx
@@ -25,8 +25,7 @@ function SelectedProduct() {
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const [productedTotalPrice, setProductedTotalPrice] = useState([]);
-  const [quantity, setQuantity] = useState([]);
-  const [cartDataId, setCartDataId] = useState([]);
+  const [reverseIndex, setReverseIndex] = useState([]);
 
   const {
     isLoading,
@@ -53,9 +52,9 @@ function SelectedProduct() {
       );
 
       setSelectedCartData(filteredData);
+      console.log('selectedCartData:', selectedCartData);
 
       if (filteredData.length > 0) {
-        setCartDataId(filteredData[0].id);
         const initialCartItems = filteredData.map((item) => ({
           ...item,
           count: item.selectedQuantity || 1,
@@ -66,70 +65,107 @@ function SelectedProduct() {
     }
   }, [isLoading, dataItems]);
 
-  useEffect(() => {
-    console.log(cartDataId);
-  }, [cartDataId]);
+  const increaseCount = async (itemIndex) => {
+    const reverseIndex = cartItems.length - 1 - itemIndex;
+    setReverseIndex(reverseIndex);
 
-  const increaseCount = (index) => {
+    const currentCartItem = cartItems[reverseIndex];
+    const updatedCount = currentCartItem.count + 1;
+    const increaseCountTotalPrice =
+      currentCartItem.selectedPrice * updatedCount;
+
+    console.log('increaseCountTotalPrice:', increaseCountTotalPrice);
+
+    const updatedCartItem = {
+      ...currentCartItem,
+      count: updatedCount,
+      selectedSubtotal: increaseCountTotalPrice,
+    };
+
+    console.log('updatedCartItem id:', updatedCartItem.id);
+    console.log('updatedCartItem:', updatedCartItem);
+
     setCartItems((prevItems) => {
       const newItems = [...prevItems];
-      newItems[index] = {
-        ...newItems[index],
-        count: newItems[index].count + 1,
-      };
+      newItems[reverseIndex] = updatedCartItem;
 
-      const increaseCountTotalPrice =
-        newItems[index]?.selectedPrice * newItems[index].count;
-
-      setProductedTotalPrice((prevPrices) => {
-        const updatedPrices = [...prevPrices];
-        if (updatedPrices.length <= index) {
-          updatedPrices.push(increaseCountTotalPrice);
-        } else {
-          updatedPrices[index] = increaseCountTotalPrice;
-        }
-        console.log('increaseCountTotalPrice:', increaseCountTotalPrice);
-        console.log('productedTotalPrice:', productedTotalPrice);
-        console.log('cartItems:', cartItems);
-        console.log('newItems:', newItems);
-
-        return updatedPrices;
-      });
+      console.log('newItems:', newItems);
 
       return newItems;
     });
+
+    await pb.collection('cart').update(updatedCartItem.id, {
+      selectedQuantity: updatedCartItem.count,
+      selectedSubtotal: increaseCountTotalPrice,
+    });
+
+    setProductedTotalPrice((prevPrices) => {
+      const updatedPrices = prevPrices.map((price, index) => {
+        if (index === itemIndex) {
+          return increaseCountTotalPrice;
+        } else {
+          return price;
+        }
+      });
+
+      console.log('updatedPrices:', updatedPrices);
+
+      return updatedPrices;
+    });
+
+    console.log(cartItems[itemIndex].count);
+
+    const currentItemID = cartItems[itemIndex]?.id;
+    console.log('index id:', currentItemID);
+
+    const cartId = cartItems[reverseIndex].id;
+    console.log('reverseIndex id:', cartId);
   };
 
-  const decreaseCount = (index) => {
-    setCartItems(async (prevItems) => {
+  const decreaseCount = async (itemIndex) => {
+    const reverseIndex = cartItems.length - 1 - itemIndex;
+    const currentCartItem = cartItems[reverseIndex];
+
+    if (currentCartItem.count <= 1) {
+      return;
+    }
+
+    const updatedCount = currentCartItem.count - 1;
+    const increaseCountTotalPrice =
+      currentCartItem.selectedPrice * updatedCount;
+
+    const updatedCartItem = {
+      ...currentCartItem,
+      count: updatedCount,
+      selectedSubtotal: increaseCountTotalPrice,
+    };
+
+    setCartItems((prevItems) => {
       const newItems = [...prevItems];
-      const newCount = newItems[index].count - 1;
-      newItems[index] = {
-        ...newItems[index],
-        count: newCount,
-      };
+      newItems[reverseIndex] = updatedCartItem;
 
-      setQuantity(newItems[index].count);
-      await pb.collection('cart').update(cartDataId, {
-        selectedQuantity: quantity,
-      });
-
-      console.log('cartItems:', cartItems);
-      console.log('newItems:', newItems);
-
-      const decreaseCountTotalPrice =
-        newItems[index].selectedPrice * newItems[index].count;
-      setProductedTotalPrice((prevPrices) => {
-        const updatedPrices = [...prevPrices];
-        if (updatedPrices.length <= index) {
-          updatedPrices.push(decreaseCountTotalPrice);
-        } else {
-          updatedPrices[index] = decreaseCountTotalPrice;
-        }
-        return updatedPrices;
-      });
+      console.log('newItems:', newItems); // Correct placement for console.log
 
       return newItems;
+    });
+
+    await pb.collection('cart').update(updatedCartItem.id, {
+      selectedQuantity: updatedCartItem.count,
+      selectedSubtotal: increaseCountTotalPrice,
+    });
+
+    setProductedTotalPrice((prevPrices) => {
+      const updatedPrices = prevPrices.map((price, index) => {
+        if (index === itemIndex) {
+          return increaseCountTotalPrice;
+        } else {
+          return price;
+        }
+      });
+
+      console.log('updatedPrices:', updatedPrices);
+
+      return updatedPrices;
     });
   };
 
@@ -157,16 +193,20 @@ function SelectedProduct() {
     setCartItems(initialCartItems);
   }, [selectedCartData]);
 
+  useEffect(() => {
+    console.log(cartItems);
+  });
+
   const calculateTotalPrice = () => {
     let calculatedTotalPrice = 0;
 
     if (Array.isArray(cartItems) && cartItems.length > 0) {
       cartItems.forEach((item) => {
         const count = item.count || item.selectedQuantity || 1;
-        calculatedTotalPrice += item.selectedPrice * count;
+        const selectedPrice = item.selectedPrice || 0;
+        calculatedTotalPrice += selectedPrice * count;
       });
     }
-
     return calculatedTotalPrice;
   };
 
@@ -179,18 +219,43 @@ function SelectedProduct() {
     console.log('Updated Total Price:', totalPrice);
   }, [totalPrice]);
 
-  const deleteItem = async (index) => {
-    const itemId = selectedCartData[index].id;
-    console.log(itemId);
+  const deleteItem = async (itemIndex) => {
+    const reverseIndex = cartItems.length - 1 - itemIndex;
+
+    if (reverseIndex < 0 || reverseIndex >= selectedCartData.length) {
+      console.log('Error: Reverse index is out of range.');
+      return;
+    }
+
+    const updatedCartItems = [...cartItems];
+
+    if (reverseIndex >= updatedCartItems.length || reverseIndex < 0) {
+      console.log('Error: Item does not exist in cartItems.');
+      console.log('updatedCartItems:', updatedCartItems);
+      return;
+    }
+
+    const currentCartItem = updatedCartItems[reverseIndex];
+
+    if (!currentCartItem || !currentCartItem.id) {
+      console.log('Error: Item or item id is undefined.');
+      console.log('cartItems:', cartItems);
+      console.log('currentCartItem:', currentCartItem);
+      return;
+    }
+
+    const itemId = currentCartItem.id;
+    console.log('delete itemId:', itemId);
 
     await pb.collection('cart').delete(itemId);
 
     let updatedCounts = [...counts];
-    updatedCounts.splice(index, 1);
+    updatedCounts.splice(reverseIndex, 1);
 
     let updatedCartData = [...selectedCartData];
-    updatedCartData.splice(index, 1);
+    updatedCartData.splice(reverseIndex, 1);
 
+    setCartItems(updatedCartItems);
     setCounts(updatedCounts);
     setSelectedCartData(updatedCartData);
   };
@@ -207,25 +272,24 @@ function SelectedProduct() {
       </div>
     );
   }
-
   return (
     <>
       <h2 className="sr-only">장바구니 페이지</h2>
       <span
-        className="text-2xl font-bold mb-[4.5rem]"
+        className="text-2xl font-bold mt-[10rem] mb-[4.5rem]"
         aria-label="장바구니"
         aria-hidden
       >
         C H E C K O U T
       </span>
       <ul className="w-[930px] flex pb-2">
-        <li className="mr-[38.5rem]">
+        <li className="ml-2 mr-[37.5rem]">
           <span className="font-semibold">Product</span>
         </li>
-        <li className="mr-7">
+        <li className="mr-9">
           <span className="font-semibold">Price</span>
         </li>
-        <li className="mr-7">
+        <li className="mr-9">
           <span className="font-semibold">Quantity</span>
         </li>
         <li>
@@ -233,28 +297,48 @@ function SelectedProduct() {
         </li>
       </ul>
       <div>
-        {selectedCartData?.toReversed().map((item, index) => {
-          if (cartItems[index]) {
-            const itemWithCount = { ...item, count: cartItems[index].count };
-            const individualProductedTotalPrice =
-              productedTotalPrice.length > index
-                ? productedTotalPrice[index]
-                : cartItems[index].selectedSubtotal;
-            return (
-              <SelectedProductItem
-                key={item.id}
-                item={itemWithCount}
-                index={index}
-                deleteItem={deleteItem}
-                individualProductedTotalPrice={individualProductedTotalPrice}
-                increaseCount={() => increaseCount(index)}
-                decreaseCount={() => decreaseCount(index)}
-              />
-            );
-          }
-        })}
+        {dataItems.length <= 0 ? (
+          <>
+            <div className="w-[960px] border-t-2 border-black ml-6"></div>
+            <div className="h-[20rem] flex flex-col justify-center items-center">
+              <span>장바구니가 비어있습니다.</span>
+            </div>
+          </>
+        ) : (
+          <>
+            {cartItems?.toReversed().map((item, index) => {
+              const cartItem = cartItems.find(
+                (cartItem) => cartItem.id === item.id
+              );
+              if (cartItem) {
+                const itemWithCount = {
+                  ...item,
+                  count: cartItem.count,
+                };
+                const reverseIndex = cartItems.length - 1 - index;
+                const individualProductedTotalPrice =
+                  productedTotalPrice.length > reverseIndex
+                    ? productedTotalPrice[reverseIndex]
+                    : cartItem.selectedSubtotal;
+                return (
+                  <SelectedProductItem
+                    key={item.id}
+                    item={itemWithCount}
+                    index={index}
+                    deleteItem={deleteItem}
+                    individualProductedTotalPrice={
+                      individualProductedTotalPrice
+                    }
+                    increaseCount={() => increaseCount(index)}
+                    decreaseCount={() => decreaseCount(index)}
+                  />
+                );
+              }
+            })}
+          </>
+        )}
       </div>
-      <div className="flex flex-col ml-[32rem] mb-10">
+      <div className="flex flex-col ml-[32rem] mt-10 mb-10">
         <span className="text-[1.25rem] font-semibold">Cart Total</span>
         <div className="w-[25rem] border-t-2 border-black"></div>
         <span className="text-[1rem] font-semibold">Subtotal</span>
@@ -262,12 +346,14 @@ function SelectedProduct() {
         <span className="text-[1rem] font-semibold">Total</span>
         <div className="w-[25rem] mb-6 border-t-2 border-black"></div>
         <div className="flex justify-end">
-          <span className="text-[1.5rem] font-semibold">{`${totalPrice}`}</span>
+          <span className="text-[1.5rem] font-semibold">
+            {`${totalPrice.toLocaleString('ko-KR')}`} 원
+          </span>
         </div>
       </div>
       <div className="ml-[32rem]">
         <Link to={`/lumieleu/order`}>
-          <button className="w-[25rem] h-[3.125rem] rounded-md text-white bg-black">
+          <button className="w-[25rem] h-[3.125rem] mb-[5rem] rounded-md text-white bg-black">
             PROCEEO TO CHECKOUT
           </button>
         </Link>
