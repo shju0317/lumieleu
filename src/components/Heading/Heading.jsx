cd97996b830df6cfa33050d63b314cc8bfdf6735
youngniiiiii <hanuje_@naver.com>
2023-10-25 23:00:42 +0900

    feat  : nav에 로그인,로그아웃 구현 , 방문자 name 노출  , 로그인시 회원가입 보이지않게하기

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,16 +1,46 @@
 import S from './Heading.module.css';
 import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import useAuthStore from '@/store/store';
 
 function Heading() {
+  /* 인증 정보에 따른 로그인 ➡️ 로그아웃으로 변경 */
+  const isAuth = useAuthStore((state) => state.isAuth);
+  console.log(isAuth);
+
+  /* 로그인 시 userName || name렌더링 */
+  const user = useAuthStore((state) => state.user);
+
+  /* 일반사용자 로그아웃 및 카카오 사용자 로그아웃 */
+  const signOut = useAuthStore((state) => state.signOut);
+  const handleSignOut = () => {
+    toast.success('정상적으로 로그아웃 되었습니다.');
+    signOut();
+    // kakaoLogout();
+  };
+
   return (
     <div className={S.headingWrapper}>
       <h1 className="text-white">헤더</h1>
-      <Link to="signin" className="text-white">
-        Sign In
-      </Link>
-      <Link to="signus" className="text-white">
-        Sign Us
-      </Link>
+      {isAuth ? (
+        <div className="text-white" onClick={handleSignOut}>
+          로그아웃
+        </div>
+      ) : (
+        <Link to="signin" className="text-white">
+          Sign In
+        </Link>
+      )}
+
+      {!isAuth && (
+        <Link to="signus" className="text-white">
+          Sign Us
+        </Link>
+      )}
+
+      {isAuth && user && (
+        <li className="text-white">{user.name || user.username}님</li>
+      )}
     </div>
   );
 }
