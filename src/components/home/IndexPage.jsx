2d6acf87ec08b4f8dd8f09ea5602394e45b8927d
dongapple <hg971027@gmail.com>
2023-10-27 23:44:08 +0900

    markup : 상실의 시작 삭제

--- src/components/home/IndexPage.jsx
+++ src/components/home/IndexPage.jsx
@@ -43,7 +43,7 @@ function IndexPage() {
       </div>
       <div className={`${S.goGallery} ${isMouseMoving ? S.moveUp : ''}`}>
         <Link to="/lumieleu/gallery" className="font-serif text-2xl">
-          상실의 시작 작품 보러가기
+          작품 보러가기
           <br />
           <span>&#8744;</span>
         </Link>
