27e7612ef9f83dbfbce47b3618beb5e5ea87ed8d
dongapple <hg971027@gmail.com>
2023-10-27 23:36:27 +0900

    init : 푸터 생성

--- src/layout/FooterBar.jsx
+++ src/layout/FooterBar.jsx
@@ -1,5 +1,57 @@
+import { Link } from 'react-router-dom';
+
 function FooterBar() {
-  return <footer>푸터 입니다</footer>;
+  return (
+    <footer className="flex justify-around text-xs">
+      <p>
+        {' '}
+        lumière <br /> de l&#39;aube
+      </p>
+      <ul>
+        <li>
+          <dl className="flex">
+            <dt>LDL STUDIO : </dt>
+            <dd>서울특별시 종로구 종로3길 17 D타워, 16-17층</dd>
+          </dl>
+        </li>
+        <li>
+          <dl className="flex">
+            <dt>TEL : </dt>
+            <dd>+82 123 4567</dd>
+          </dl>
+        </li>
+        <li>
+          <dl className="flex">
+            <dt>OPEN : </dt>
+            <dd>10 AM - 6 PM</dd>
+          </dl>
+        </li>
+        <li>
+          <dl className="flex">
+            <dt>MAIL : </dt>
+            <dd>Info@naver.com</dd>
+          </dl>
+        </li>
+      </ul>
+      <dl>
+        <dt>DEVELOPER</dt>
+        <dd>Bang Seo Bin</dd>
+        <dd>Shin Dong Hyuk</dd>
+        <dd>Song Young Eun</dd>
+        <dd>Chung Min Jee</dd>
+        <dd>Shin Hyun Ju</dd>
+      </dl>
+
+      <dl>
+        <dt>PHOTO BY</dt>
+        <dd>Chung Min Jee</dd>
+      </dl>
+
+      <Link>
+        <img src="" alt="git" />
+      </Link>
+    </footer>
+  );
 }
 
 export default FooterBar;
