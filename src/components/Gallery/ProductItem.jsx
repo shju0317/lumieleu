9f30c7d1be68e119066be70df1568c96c6e57489
dongapple <hg971027@gmail.com>
2023-10-27 15:17:53 +0900

    design : 프로덕트 아이템 스타일 변경
    - 프롭스로 인덱스와 데이터 전달받기.
    - 이름과 가격, 이미지 반영.
    - 짝수 번의 아이템에 마진값 지정.

--- src/components/Gallery/ProductItem.jsx
+++ src/components/Gallery/ProductItem.jsx
@@ -1,11 +1,29 @@
-function ProductItem() {
+import { getPbImageURL } from '@/utils';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function ProductItem({ products, index }) {
+  const isOddIndex = index % 2 !== 0;
+
   return (
-    <div>
-      <h3 className="text-[80px]">01</h3>
-      <img src="" alt="" className="w-[284px] h-[360px]" />
-      <p>Bonorum et Malorum, written</p>
+    <div style={{ marginTop: isOddIndex ? '144px' : '0' }}>
+      <Link to={`/lumieleu/gallery/${products.id}`}>
+        <h3 className="text-[80px]">{`0${index + 1}`}</h3>
+        <img
+          src={getPbImageURL(products, 'image')}
+          alt={products.title}
+          className="w-full "
+        />
+        <p>{products.title}</p>
+        <p>{products.price}</p>
+      </Link>
     </div>
   );
 }
+
+ProductItem.propTypes = {
+  products: PropTypes.object,
+  index: PropTypes.number,
+};
 
 export default ProductItem;
