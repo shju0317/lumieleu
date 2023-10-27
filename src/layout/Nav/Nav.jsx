0df3defedcc93cbc3ef5fd38ef8038bc6ab676ca
dongapple <hg971027@gmail.com>
2023-10-27 16:46:01 +0900

    markup : 첫 화면에 로그아웃이 아니라 로그인 생성

--- src/layout/Nav/Nav.jsx
+++ src/layout/Nav/Nav.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Nav() {
   const location = useLocation();
 
-  const categories = ['ABOUT', 'GALLERY', 'CART', 'MYPAGE', 'LOGOUT'];
+  const categories = ['ABOUT', 'GALLERY', 'CART', 'MYPAGE', 'SIGNIN'];
 
   return (
     <nav>
