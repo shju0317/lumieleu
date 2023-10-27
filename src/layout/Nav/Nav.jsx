5f626b09f386c5db3d47b36ac14fb15e33a9d81f
dongapple <hg971027@gmail.com>
2023-10-27 15:14:01 +0900

    design : 선택된 페이지 표시
    - Nav에서 현제 페이지 아래 언더바 표시 추가

--- src/layout/Nav/Nav.jsx
+++ src/layout/Nav/Nav.jsx
@@ -1,15 +1,10 @@
 // import S from './Nav.module.css';
-import { PropTypes } from 'prop-types';
-import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
-function Nav({ onCategorySelect }) {
-  const [selectedCategory, setSelectedCategory] = useState('전체');
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
-    onCategorySelect(category);
-  };
+function Nav() {
+  const location = useLocation();
 
   const categories = ['ABOUT', 'GALLERY', 'CART', 'MYPAGE', 'LOGOUT'];
 
@@ -20,9 +15,8 @@ function Nav({ onCategorySelect }) {
           <li key={category} className="">
             <Link
               to={`/lumieleu/${category.toLowerCase()}`}
-              onClick={() => handleCategoryClick(category)}
               className={` py-2  ${
-                selectedCategory === category
+                location.pathname.toUpperCase().includes(category)
                   ? 'border-b-4 pb-1 box-border border-black '
                   : ''
               } `}
@@ -37,7 +31,3 @@ function Nav({ onCategorySelect }) {
 }
 
 export default Nav;
-
-Nav.propTypes = {
-  onCategorySelect: PropTypes.func.isRequired,
-};
