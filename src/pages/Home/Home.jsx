4a22bab884a470fa7eb840062920f09dd460cffb
dongapple <hg971027@gmail.com>
2023-10-27 22:12:40 +0900

    markup : 인덱스 페이지 변경

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,9 @@
-import { useEffect } from 'react';
 import IndexPage from '@/components/home/IndexPage';
-import GalleryPage from '@/components/home/GalleryPage';
-import pageableMin from 'pageable';
 
 function Home() {
-  useEffect(() => {
-    const container = document.querySelector('#container');
-    new pageableMin(container, {
-      anchors: ['home', 'gallery'],
-    });
-  }, []);
-
   return (
     <section className="relative" id="container">
-      <div>
-        <div data-anchor="home">
-          <IndexPage />
-        </div>
-        <div data-anchor="gallery">
-          <GalleryPage />
-        </div>
-      </div>
+      <IndexPage />
     </section>
   );
 }
