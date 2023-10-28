506cba710b5c7422df8af50862bdebf05a579597
youngniiiiii <hanuje_@naver.com>
2023-10-28 22:52:50 +0900

    feat  : categories가 LOGIN일 경우 필터, 맵으로  로그아웃버튼, 닉네임 나타내기

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -2,25 +2,8 @@ import Nav from '@/layout/Nav/Nav';
 import S from './Heading.module.css';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import toast from 'react-hot-toast';
-import useAuthStore from '@/store/store';
 
 function Heading() {
-  /* 인증 정보에 따른 로그인 ➡️ 로그아웃으로 변경 */
-  const isAuth = useAuthStore((state) => state.isAuth);
-  // console.log(isAuth);
-
-  /* 로그인 시 userName || name렌더링 */
-  const user = useAuthStore((state) => state.user);
-
-  /* 일반사용자 로그아웃 및 카카오 사용자 로그아웃 */
-  const signOut = useAuthStore((state) => state.signOut);
-  const handleSignOut = () => {
-    toast.success('정상적으로 로그아웃 되었습니다.', { duration: 1000 });
-    signOut();
-    // kakaoLogout();
-  };
-
   const location = useLocation();
 
   return (
@@ -38,18 +21,6 @@ function Heading() {
         lumière <br /> de l&#39;aube
       </Link>
       <Nav className={location.hash === '#home' ? S.navTextWhite : ''} />
-
-      {/* {isAuth ? (
-        <div className=" cursor-pointer" onClick={handleSignOut}>
-          로그아웃
-        </div>
-      ) : (
-        <Link to="signin">Sign In</Link>
-      )}
-
-      {!isAuth && <Link to="signup">Sign Up</Link>}
-
-      {isAuth && user && <li>{user.name || user.username}님</li>} */}
     </div>
   );
 }
