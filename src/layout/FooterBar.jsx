f7f7647ac0b8c0d07337fa1adfe401d3c7a0c2f3
dongapple <hg971027@gmail.com>
2023-10-31 13:14:32 +0900

    markup : 푸터 마크업 및 간단한 css

--- src/layout/FooterBar.jsx
+++ src/layout/FooterBar.jsx
@@ -1,40 +1,41 @@
 import { Link } from 'react-router-dom';
+import gitImg from '/git.svg';
 
 function FooterBar() {
   return (
-    <footer className="flex justify-around text-xs">
-      <p>
-        {' '}
+    <footer className="flex justify-around text-xs py-[30px] ali">
+      <p className="font-bold text-center ">
         lumière <br /> de l&#39;aube
       </p>
-      <ul>
+      <ul className="flex flex-col gap-1">
         <li>
-          <dl className="flex">
+          <dl className="flex gap-1 ">
             <dt>LDL STUDIO : </dt>
             <dd>서울특별시 종로구 종로3길 17 D타워, 16-17층</dd>
           </dl>
         </li>
         <li>
-          <dl className="flex">
+          <dl className="flex gap-1">
             <dt>TEL : </dt>
             <dd>+82 123 4567</dd>
           </dl>
         </li>
         <li>
-          <dl className="flex">
+          <dl className="flex gap-1">
             <dt>OPEN : </dt>
             <dd>10 AM - 6 PM</dd>
           </dl>
         </li>
         <li>
-          <dl className="flex">
+          <dl className="flex gap-1">
             <dt>MAIL : </dt>
             <dd>Info@naver.com</dd>
           </dl>
         </li>
       </ul>
-      <dl>
-        <dt>DEVELOPER</dt>
+
+      <dl className="grid grid-cols-3 grid-rows-3 gap-x-2 h-[60px]">
+        <dt className="row-span-3">DEVELOPER</dt>
         <dd>Bang Seo Bin</dd>
         <dd>Shin Dong Hyuk</dd>
         <dd>Song Young Eun</dd>
@@ -42,13 +43,13 @@ function FooterBar() {
         <dd>Shin Hyun Ju</dd>
       </dl>
 
-      <dl>
+      <dl className="flex gap-5">
         <dt>PHOTO BY</dt>
         <dd>Chung Min Jee</dd>
       </dl>
 
       <Link>
-        <img src="" alt="git" />
+        <img src={gitImg} alt="git" />
       </Link>
     </footer>
   );
