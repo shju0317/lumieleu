389ba45410c087039ff038ee8ea25b18e877c879
shju0317 <shju0317@naver.com>
2023-11-06 19:42:53 +0900

    style: 마이페이지 padding 수정

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -3,7 +3,7 @@ import OrderedProductList from './../../components/MyPage/OrderedProductList';
 
 function MyPage() {
   return (
-    <div>
+    <div className="py-[66px]">
       <Profile/>
       <section className="flex flex-col gap-3 mx-auto max-w-3xl mt-6">
         <h2 className="text-lg font-semibold">주문 내역</h2>
