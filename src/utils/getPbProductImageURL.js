db9ce776aa51d042cf8b80ee72a15c94b8f1be45
seobinbang7 <newmso5382@gmail.com>
2023-11-06 18:03:30 +0900

    기능 추가

--- src/utils/getPbProductImageURL.js
+++ src/utils/getPbProductImageURL.js
@@ -4,7 +4,8 @@
 
 const PB = import.meta.env.VITE_PB_URL;
 
-export const getPbImageURL = (item, fileNames = ['photo']) => {
+// https://lumieleu.pockethost.io/api/files/4cqao3e5z0e1rfi/4qw42es34mvu7ma/9_title_xp9cJG1J9k.jpg
+export const getPbProductImageURL = (item, fileNames = ['image']) => {
   if (!Array.isArray(fileNames)) {
     fileNames = [fileNames];
   }
@@ -13,12 +14,14 @@ export const getPbImageURL = (item, fileNames = ['photo']) => {
 
   fileNames.forEach((fileName) => {
     if (Array.isArray(item[fileName])) {
-      item[fileName].forEach((photo) => {
-        urls.push(`${PB}/api/files/${item.collectionId}/${item.id}/${photo}`);
+      item[fileName].forEach((image) => {
+        urls.push(
+          `${PB}/api/files/${item.expand.product.collectionId}/${item.expand.product.id}/${image}`
+        );
       });
     } else {
       urls.push(
-        `${PB}/api/files/${item.collectionId}/${item.id}/${item[fileName]}`
+        `${PB}/api/files/${item.expand.product.collectionId}/${item.expand.product.id}/${fileName}`
       );
     }
   });
